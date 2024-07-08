3275b1b951ab11c97ecf9012f52834b1bb946f7f
Amir Zur <amir.zur@hotmail.com>
2024-07-08 10:32:32 -0700

    try bert for classification training

--- train_classifier.sh
+++ train_classifier.sh
@@ -1,5 +1,5 @@
 python train_classifier.py \
-    --model_name_or_path "bert-base" \
+    --model_name_or_path "bert-base-uncased" \
     --dataset_split "train_inclusive" \
     --output_dir "train_classifier" \
     --per_device_train_batch_size 32 \
