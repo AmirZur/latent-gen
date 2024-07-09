5000c2b2c7355c5c9af8a62d83763b97a2ff5f11
Amir Zur <amir.zur@hotmail.com>
2024-07-09 09:14:08 -0700

    reduce batch size

--- instruct_tune.sh
+++ instruct_tune.sh
@@ -2,8 +2,8 @@ python instruct_tune.py \
     --model_name_or_path "microsoft/Phi-3-mini-4k-instruct" \
     --dataset_split "train_observational" \
     --output_dir "inst_tune" \
-    --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 32 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --num_train_epochs 3 \
     --learning_rate 1e-5 \
     --logging_dir "logs" \
