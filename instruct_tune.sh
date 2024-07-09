39878c961c1fc1c3c74413ac61d8094144255714
Amir Zur <amir.zur@hotmail.com>
2024-07-09 09:15:15 -0700

    revert instruct tune, reduce batch size for classifier training

--- instruct_tune.sh
+++ instruct_tune.sh
@@ -1,11 +1,12 @@
 python instruct_tune.py \
     --model_name_or_path "microsoft/Phi-3-mini-4k-instruct" \
-    --dataset_split "train_observational" \
+    --dataset_split "train_inclusive" \
     --output_dir "inst_tune" \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
-    --num_train_epochs 3 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
+    --num_train_epochs 5 \
     --learning_rate 1e-5 \
     --logging_dir "logs" \
     --logging_steps 10 \
-    --max_seq_length 128
+    --max_seq_length 256 \
+    --save_model
--- train_classifier.sh
+++ train_classifier.sh
@@ -2,8 +2,8 @@ python train_classifier.py \
     --model_name_or_path "meta-llama/Llama-2-7b-hf" \
     --dataset_split "train_inclusive" \
     --output_dir "train_classifier" \
-    --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 32 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --num_train_epochs 3 \
     --learning_rate 1e-5 \
     --logging_dir "logs" \
