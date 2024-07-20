a064ddf6e13b5b4132ffcc4b3987e16c2b42d640
Amir Zur <amir.zur@hotmail.com>
2024-07-20 10:19:14 -0700

    das eval for toy example

---
+++ das_toy_eval.sh
@@ -0,0 +1,10 @@
+python das_eval.py \
+    --model_name_or_path "gpt2" \
+    --das_path "das/gpt2/2024-07-19-18-16-42/weights" \
+    --toy_dataset \
+    --positions "l1" \
+    --share_weights \
+    --intervention_offset 0 \
+    --output_dir "das_eval" \
+    --max_new_tokens 8 \
+    --batch_size 8
