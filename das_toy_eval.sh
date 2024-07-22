c0915468be6c63a9eb000db4c5900ec4588badfa
Amir Zur <amir.zur@hotmail.com>
2024-07-22 12:49:06 -0700

    eval das model trained directly on name token

--- das_toy_eval.sh
+++ das_toy_eval.sh
@@ -1,8 +1,8 @@
 python das_eval.py \
     --model_name_or_path "gpt2" \
-    --das_path "das/gpt2/2024-07-20-11-11-06/weights" \
+    --das_path "das/gpt2/2024-07-22-12-27-25/weights" \
     --toy_dataset \
-    --positions "l1" \
+    --positions "f4" \
     --share_weights \
     --intervention_offset 0 \
     --output_dir "das_eval" \
