ff82f1a653a8bd3e03ffb81a56f60504f2af1b67
Amir Zur <amir.zur@hotmail.com>
2024-07-22 14:47:09 -0700

    try das on cebab for longer training

--- das.sh
+++ das.sh
@@ -1,4 +1,4 @@
-for i in 0 2 4 6 8 10 12 14 16 18 20
+for i in 0 5 10 15 20
 do
     python das.py \
         --model_name_or_path "inst_tune" \
@@ -8,7 +8,7 @@ do
         --intervention_offset $i \
         --share_weights \
         --dropout 0 \
-        --num_train_epochs 3 \
+        --num_train_epochs 10 \
         --learning_rate 1e-3 \
         --batch_size 8 \
         --max_seq_length 256 \
