3208903798e6578a1849b814b73605d919dd2e6a
Amir Zur <amir.zur@hotmail.com>
2024-07-25 16:55:01 -0700

    merge generate examples keyword args into single bash file

--- generate_examples.sh
+++ generate_examples.sh
@@ -4,3 +4,11 @@ python generate_examples.py \
     --use_flash_attention \
     --do_sample \
     --temperature 0.7
+
+python generate_examples.py \
+    --model_name_or_path "inst_tune" \
+    --output_dir "inst_gens" \
+    --use_flash_attention \
+    --do_sample \
+    --temperature 0.7 \
+    --prefill_generation
