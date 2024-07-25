9b882605b9feee2db54bb51c863ffc970eb1f674
Amir Zur <amir.zur@hotmail.com>
2024-07-25 10:40:35 -0700

    generate multiple examples per restaurant metadata

--- generate_examples.py
+++ generate_examples.py
@@ -3,7 +3,8 @@ import json
 import random
 import os
 import torch
-from tqdm import tqdm
+from tqdm import trange
+import pandas as pd
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 
@@ -21,18 +22,18 @@ def create_example(example):
     # convert string to dict (VERY UNSAFE, DO NOT USE ON UNTRUSTED DATA)
     metadata = eval(example['opentable_metadata'])
     prompt = PROMPT_TEMPLATE.format(**metadata)
-    completion = example['description']
     messages = [
         {'role': 'user', 'content': prompt},
-        {'role': 'assistant', 'content': completion}
     ]
     return {'messages': messages}
 
 def main(
-    model_name_or_path: str = "microsoft/Phi-3-mini-4k-instruct",
+    model_name_or_path: str = "inst_tune",
     output_dir: str = "inst_gens",
-    num_examples: int = 100,
+    num_generations_per_example: int = 10,
+    batch_size: int = 8,
     max_new_tokens: int = 256,
+    dataset_split: str = "train_observational",
     **generate_kwargs
 ):
     # Load the model and tokenizer
@@ -48,81 +49,46 @@ def main(
         tokenizer.pad_token = tokenizer.eos_token
 
     # Load the dataset
-    train_dataset = load_dataset("CEBaB/CEBaB", split="train_inclusive")
-    train_dataset = train_dataset.map(create_example)
-    
-    original_examples = train_dataset.filter(
-        lambda x: x['is_original']
-    ).shuffle(seed=SEED).select(range(num_examples))
+    train_dataset = load_dataset("CEBaB/CEBaB", split=dataset_split)
+    df = pd.DataFrame(train_dataset)
+    df['restaurant_id'] = df['opentable_metadata'].map(lambda x: eval(x)['restaurant_id'])
+    df = df.drop_duplicates(subset='restaurant_id').reset_index(drop=True)
 
-    edited_examples = train_dataset.filter(
-        lambda x: x['original_id'] in original_examples['original_id']
-    )
-
-    def generate_example(example):
+    data = []
+    generations = []
+    for b in trange(0, len(df), batch_size, desc="Generating..."):
+        batch = df.iloc[b:b+batch_size].to_dict(orient="records")
+        examples = [create_example(example) for example in batch]
         inputs = tokenizer.apply_chat_template(
-            example['messages'][:1],
+            examples,
             return_tensors="pt",
             add_generation_prompt=True
         ).to(device)
         with torch.no_grad():
-            generation = model.generate(
+            outputs = model.generate(
                 input_ids=inputs, 
                 max_new_tokens=max_new_tokens, 
                 pad_token_id=tokenizer.eos_token_id,
+                num_return_sequences=num_generations_per_example,
                 **generate_kwargs
             )
-        return {
-            'generation': tokenizer.decode(generation[0])
-        }
-
-    original_examples = original_examples.map(
-        generate_example, desc="Generating model completions..."
-    )
-
-    def compute_example_perplexity(example):
-        messages = example['messages']
-        inputs = tokenizer.apply_chat_template(
-            messages,
-            return_tensors="pt"
-        ).to(device)
-        with torch.no_grad():
-            perplexity = torch.exp(
-                model(input_ids=inputs, labels=inputs).loss
-            ).item()
-        return {
-            'description_perplexity': perplexity
-        }
-
-    edited_examples = edited_examples.map(
-        compute_example_perplexity, desc="Computing description perplexities..."
-    )
-
-    def compute_output_perplexity(example):
-        inputs = tokenizer(example['generation'], return_tensors="pt").to(device)
-        with torch.no_grad():
-            perplexity = torch.exp(
-                model(**inputs, labels=inputs['input_ids']).loss
-            ).item()
-        return {
-            'generation_perplexity': perplexity
-        }
-
-    original_examples = original_examples.map(
-        compute_output_perplexity, desc="Computing generation perplexities..."
-    )
+            generations += tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            data += [b for b in batch for _ in range(num_generations_per_example)]
+    
+    df = pd.DataFrame(data)
+    df['generation'] = generations
 
     os.makedirs(output_dir, exist_ok=True)
-    
-    original_examples.save_to_disk(os.path.join(output_dir, "original_examples"))
-    edited_examples.save_to_disk(os.path.join(output_dir, "edited_examples"))
+    df.to_csv(os.path.join(output_dir, "generations.csv"), index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name_or_path", type=str, default="microsoft/Phi-3-mini-4k-instruct")
+    parser.add_argument("--model_name_or_path", type=str, default="inst_tune")
     parser.add_argument("--output_dir", type=str, default="inst_gens")
-    parser.add_argument("--num_examples", type=int, default=100)
+    parser.add_argument("--num_generations_per_example", type=int, default=10)
     parser.add_argument("--max_new_tokens", type=int, default=256)
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--dataset_split", type=str, default="train_observational")
     # generation arguments
     parser.add_argument("--do_sample", action="store_true")
     parser.add_argument("--temperature", type=float, default=0.7)
