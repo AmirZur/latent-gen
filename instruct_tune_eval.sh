fe294c95b42db4badee7209b8fd190180f836720
Amir Zur <amir.zur@hotmail.com>
2024-07-29 09:49:46 -0700

    re-run instruction tune evaluation without prefilling

--- instruct_tune_eval.sh
+++ instruct_tune_eval.sh
@@ -1,15 +1,17 @@
-python instruct_tune.py \
-    --do_eval \
-    --do_classify \
-    --model_name_or_path "inst_tune/train" \
-    --use_flash_attention \
-    --prompt_with_example \
-    --eval_split "train_inclusive" \
-    --output_dir "inst_tune" \
-    --per_device_eval_batch_size 8 \
-    --prefill_generation \
-    --aspect "service" \
-    --max_new_tokens 256 \
-    --num_return_sequences 10 \
-    --do_sample \
-    --temperature 0.7
+for split in "validation" "train_inclusive"
+do
+    python instruct_tune.py \
+        --do_eval \
+        --do_classify \
+        --model_name_or_path "inst_tune/train" \
+        --use_flash_attention \
+        --prompt_with_example \
+        --eval_split $split \
+        --output_dir "inst_tune" \
+        --per_device_eval_batch_size 8 \
+        --aspect "service" \
+        --max_new_tokens 256 \
+        --num_return_sequences 10 \
+        --do_sample \
+        --temperature 0.7
+done
