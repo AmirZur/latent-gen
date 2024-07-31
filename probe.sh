49487c2c7bf1b97e0a89dd1180bc685df8b7f17d
Amir Zur <amir.zur@hotmail.com>
2024-07-31 09:19:00 -0700

    run probing on last 10 tokens

--- probe.sh
+++ probe.sh
@@ -1,4 +1,5 @@
 python probe.py \
     --model_name_or_path "inst_tune/train" \
     --use_flash_attention \
-    --output_dir "inst_tune/probe"
+    --output_dir "inst_tune/probe" \
+    --num_tokens_from_end 10
