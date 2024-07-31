81b3ccca6e6b2ffb70cf5d26dc345fcf52c68e72
Amir Zur <amir.zur@hotmail.com>
2024-07-30 19:08:57 -0700

    try searching over many layers at once?

---
+++ das_overkill.sh
@@ -0,0 +1,21 @@
+python das.py \
+    --model_name_or_path "inst_tune/train" \
+    --use_flash_attention \
+    --layers "20;22;24;26;28;30" \
+    --subspace_dim 128 \
+    --positions "l1" \
+    --share_weights \
+    --dropout 0 \
+    --intervention_offset 0 \
+    --prompt_with_example \
+    --num_train_epochs 5 \
+    --learning_rate 1e-3 \
+    --batch_size 8 \
+    --max_seq_length 256 \
+    --max_new_tokens 256 \
+    --do_sample \
+    --temperature 0.7 \
+    --logging_steps 10 \
+    --output_dir "das" \
+    --save_model \
+    --use_wandb
