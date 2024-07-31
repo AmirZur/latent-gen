a398f4bdb1cdf3f79501d86220c41efa028d80dc
Amir Zur <amir.zur@hotmail.com>
2024-07-31 09:46:39 -0700

    try to pass in pos variable in launch command

--- das_multiple_launch.sh
+++ das_multiple_launch.sh
@@ -1,4 +1,4 @@
 for pos in 1 2 3 4 5 6 7 8
 do
-    nlprun -a pyvene -g 1 -r 40G -q sphinx -o "logs/das_multiple_${pos}.log" 'bash das_multiple.sh ${pos}'
+    nlprun -a pyvene -g 1 -r 40G -q sphinx -o "logs/das_multiple_$pos.log" 'bash das_multiple.sh '"$pos"
 done
