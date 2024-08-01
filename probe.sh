be329e6b65948b28d76b5d9217350145332a5b6a
Amir Zur <amir.zur@hotmail.com>
2024-08-01 10:00:15 -0700

    run probe on vanilla model

--- probe.sh
+++ probe.sh
@@ -1,5 +1,7 @@
 python probe.py \
     --model_name_or_path "inst_tune/train" \
     --use_flash_attention \
-    --output_dir "inst_tune/probe" \
+    --train_path "inst_tune/vanilla_train/eval/generations.csv" \
+    --validation_path "inst_tune/vanilla_valid/eval/generations.csv" \
+    --output_dir "inst_tune/probe_vanilla" \
     --num_tokens_from_end 10
